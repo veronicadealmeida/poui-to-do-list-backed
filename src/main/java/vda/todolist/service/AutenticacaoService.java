8e2d380bfb4fdb809bd41e9f7f57abce5a4587a1
Veronica <almeida.vrc@gmail.com>
2023-09-28 06:34:26 -0300

    Adiciona configuraçao PRD

---
+++ src/main/java/vda/todolist/service/AutenticacaoService.java
@@ -0,0 +1,19 @@
+package vda.todolist.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import vda.todolist.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
